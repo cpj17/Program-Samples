2b90030473eed6d054650848bde778970a381cb5
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-02-15 09:34:22 +0530

    Add files via upload

---
+++ Files/CSharp/Excel to dataset in c#.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace ExcelToDataSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //this excel have 2 sheets main one is sheet
+                string stringPath = @"C:\Users\GSIAD-031\Desktop\Portal Data required v1.xlsx";
+
+                string filePath = stringPath;
+                string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=\"Excel 12.0;HDR=YES;IMEX=1;\"";
+                // The "HDR=YES" option indicates that the first row of the worksheet contains column headers.
+                // The "IMEX=1" option tells the provider to always read intermixed data types as text.
+
+                OleDbConnection connection = new OleDbConnection(connectionString);
+                connection.Open();
+
+                DataTable schemaTable = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                string sheetName = schemaTable.Rows[0]["TABLE_NAME"].ToString();
+                sheetName = schemaTable.Rows[1]["TABLE_NAME"].ToString(); //here index 1 is sheet1
+                //sheetName = "Sheet1$";
+                // This gets the name of the first worksheet in the workbook.
+
+                OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM [" + sheetName + "]", connection);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ Files/CSharp/split text file based on the string.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Linq;
+
+namespace Test002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable objDataTable = new DataTable();
+            objDataTable.Columns.Add("Time", typeof(string));
+            objDataTable.Columns.Add("msCallingApp", typeof(string));
+            objDataTable.Columns.Add("wsPatientID", typeof(string));
+            Console.Title = "Made By CPJ";
+            Console.WriteLine("Enter Path");
+            string filePath = Console.ReadLine();
+            string[] allLines = File.ReadAllLines(filePath);
+            int n = allLines.Length + 1;
+            int loopcount = n / 10;
+
+            string[] arr = allLines.Take(10).ToArray();
+
+            DataRow objRow = objDataTable.NewRow();
+            objRow["Time"] = arr[1];
+            objRow["msCallingApp"] = arr[7].Replace("<msCallingApp>", "").Replace("</msCallingApp>", "").Trim();
+            objRow["wsPatientID"] = arr[8].Replace("<wsPatientID>", "").Replace("</wsPatientID>", "").Trim();
+            objDataTable.Rows.Add(objRow);
+
+            for (int i = 1; i < loopcount; i++)
+            {
+                DataRow row = objDataTable.NewRow();
+                string[] arr2 = allLines.Skip(i * 10).Take(10).ToArray();
+
+                row["Time"] = arr2[1];
+                row["msCallingApp"] = arr2[7].Replace("<msCallingApp>", "").Replace("</msCallingApp>", "").Trim();
+                row["wsPatientID"] = arr2[8].Replace("<wsPatientID>", "").Replace("</wsPatientID>", "").Trim();
+                objDataTable.Rows.Add(row);
+            }
+            DataSet ds = new DataSet();
+            ds.Tables.Add(objDataTable);
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            ds.WriteXml(documentsPath + "/result.xml");
+            Console.WriteLine("export successfully");
+            Console.WriteLine("press enter to exit");
+            Console.ReadKey();
+        }
+    }
+}
