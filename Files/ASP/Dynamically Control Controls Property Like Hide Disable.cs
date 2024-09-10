214c0bdfe1bc3b6b207dfe4193bb588c0f86202a
Praveen Jeeva <78692909+cpj17@users.noreply.github.com>
2024-09-10 16:52:03 +0530

    Add files via upload

---
+++ Files/ASP/Dynamically Control Controls Property Like Hide Disable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ASPControlUtils
+{
+    public class ClsASPUtils
+    {
+        public static void DisableASPControls(params object[] objControls)
+        {
+            SetControlProperty("Enabled", false, objControls);
+        }
+
+        public static void EnableASPControls(params object[] objControls)
+        {
+            SetControlProperty("Enabled", true, objControls);
+        }
+
+        public static void ShowASPControls(params object[] objControls)
+        {
+            SetControlProperty("Visible", true, objControls);
+        }
+
+        public static void HideASPControls(params object[] objControls)
+        {
+            SetControlProperty("Visible", false, objControls);
+        }
+
+		//public static void SetControlProperty(string propertyName, bool value, params object[] objControls)  //this also working
+        public static void SetControlProperty(string propertyName, bool value, object[] objControls)
+        {
+            foreach (object objControl in objControls)
+            {
+                Type controlType = objControl.GetType();
+                var property = controlType.GetProperty(propertyName);
+                if (property != null && property.CanWrite)
+                {
+                    property.SetValue(objControl, value);
+                }
+            }
+        }
+
+        private static void ShowAlertMessage(string message)
+        {
+            // Implement your alert message logic here
+            Console.WriteLine(message); // Placeholder implementation
+        }
+    }
+}
